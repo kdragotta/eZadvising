e729b97b17c490e689958c0955d9f4fd0d056b43
John <jtbertzfi@g.coastal.edu>
2015-09-10 13:53:47 -0400

    updated login page: added php code, and table for user input.

--- login.php
+++ login.php
@@ -6,8 +6,43 @@
  * Time: 1:36 PM
  */
 
+$pagetitle = "Login";
+require_once 'config.php';
+
+if(isset($_POST['submit'])) {
+    //cleanse the data entered
+    $FORMFIELD['username'] = trim($_POST['username']);
+    $FORMFIELD['password'] = trim($_POST['password']);
+
+    //check for empty fields
+
+
+    //display errors
+
+    //get username and salt from database
+}
+
 
 
 ?>
 
-<p>This is a test file!</p>
+<p>Please log in to access registration.</p>
+
+<form name="loginForm" id="loginForm" method="post" action="login.php">
+    <table>
+        <tr>
+            <td>Username:</td>
+            <td><input type="text" name="username" id="username" size="20"/></td>
+        </tr>
+        <tr>
+            <td>Password:</td>
+            <td><input type="password" name="password" id="password" size="20"/></td>
+        </tr>
+        <tr>
+            <td>Submit:</td>
+            <td><input type="submit" name="submit" value="Submit"/></td>
+        </tr>
+    </table>
+
+
+</form>
