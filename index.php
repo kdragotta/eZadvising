2625e1bb0da231eb357ac3c0eb606810a487a468
Billy <GetSomePho@iCloud.com>
2015-10-13 07:05:17 -0400

    Deleted Usage of PlanTitleController & Model

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 require_once 'app/controller/PlanController.php';
 require_once 'app/controller/PlanItemController.php';
-require_once 'app/controller/PlanTitleController.php';
 require_once 'app/controller/StudentController.php';
 
 $planController = new PlanController();
@@ -9,9 +8,6 @@ $planController->handleRequest();
 
 $planItemController = new PlanItemController();
 $planItemController->handleRequest();
-
-//$planTitleController = new planTitleController();
-//$planTitleController->handleRequest();
 
 $studentController = new StudentController();
 $studentController->handleRequest();
