bb8f084e280d7e364a1fd73413e153eb2526a367
Kevin <floreskev@yahoo.com>
2015-10-13 12:29:13 -0400

    css added to login and register

--- eligibleNow.php
+++ eligibleNow.php
@@ -1,11 +1,45 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: John
- * Date: 9/13/2015
- * Time: 6:02 PM
- */
+echo 'This will be the eligible now feature/function<br>';
+$domain = 'mysql:dbname=ezadvising;host=localhost';
+$user = 'advising';
+$password = 'adv123';
 
-echo 'This will be the eligible now feature/function';
+try {
+    $conn = new PDO($domain, $user, $password);
+    //echo "Connection was good.<br>";
+} catch (PDOException $e) {
+    echo $user . " :: " . $password . "\n";
+    echo 'Connection failed: ' . $e->getMessage();
+}
+
+echo "<select id = 's1'>";
+//echo "<option>Select Department</option>";
+$courseInfo = "CSCI";
+echo "<option >CSCI</option>";
+echo "<option>ENGL</option>";
+echo "</select>";
+class TableRows extends RecursiveIteratorIterator {
+    function __construct($it) {
+        parent::__construct($it, self::LEAVES_ONLY);
+    }
+    function beginChildren() {
+        echo "<option>";
+    }
+    function endChildren() {
+        echo "</option>";
+    }
+}
+echo "<select>";
+echo "<option>Select Course</option>";
+$stmt = $conn->prepare("SELECT title FROM courses WHERE dept = 'document.getElementById('s1').value'");
+$stmt->execute();
+
+$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+    echo $v;
+}
+echo "</select>";
+
+
 
 ?>
