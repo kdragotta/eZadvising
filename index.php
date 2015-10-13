125bc8cf4f87814255e2b4f8ced10463de159d32
Billy <GetSomePho@iCloud.com>
2015-10-13 02:59:07 -0400

    Added handles for plan title

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php
-require_once 'app/controller/PlanItemController.php';
 require_once 'app/controller/PlanController.php';
-require_once 'app/controller/StudentController.php';
+require_once 'app/controller/PlanItemController.php';
 require_once 'app/controller/PlanTitleController.php';
+require_once 'app/controller/StudentController.php';
+
+$planController = new PlanController();
+$planController->handleRequest();
 
 $planItemController = new PlanItemController();
 $planItemController->handleRequest();
 
-$planController = new PlanController();
-$planController->handleRequests();
+//$planTitleController = new planTitleController();
+//$planTitleController->handleRequest();
 
 $studentController = new StudentController();
 $studentController->handleRequest();
-
-//$planTitleController = new PlanTitleController();
-//$planTitleController->handleRequest();
 
 ?>
