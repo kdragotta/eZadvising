2359440d7f9912fa78b390c2598f575db2252b14
Billy Le <GetSomePho@iCloud.com>
2015-10-23 01:15:26 -0400

    Intellij auto formatted

--- index.php
+++ index.php
@@ -4,34 +4,28 @@ require_once 'app/controller/PlanItemController.php';
 require_once 'app/controller/StudentController.php';
 
 
-if(isset($_POST['op']) == true)
-{
+if (isset($_POST['op']) == true) {
     $op = $_POST['op'];
 
-    if(strcmp($op, 'plan') == 0) {
+    if (strcmp($op, 'plan') == 0) {
         $planController = new PlanController();
         $planController->handleRequest();
-    }
-    else if(strcmp($op, 'planitem') == 0) {
+    } else if (strcmp($op, 'planitem') == 0) {
         $planItemController = new PlanItemController();
         $planItemController->handleRequest();
-    }
-    else if(strcmp($op, 'student') == 0) {
+    } else if (strcmp($op, 'student') == 0) {
         $studentController = new StudentController();
         $studentController->handleRequest();
     }
 }
 
 
-if(empty($_GET) && empty($_POST))
-{
+if (empty($_GET) && empty($_POST)) {
     session_start();
     $_SESSION['username'] = "crystal";
     $_SESSION['studentId'] = 1;
     $_SESSION['token'] = "ABC";
 
-    include __DIR__."/app/view/eatouch4.php";
+    include __DIR__ . "/app/view/eatouch4.php";
 }
-
-
 ?>
