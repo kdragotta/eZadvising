52b433211e8b04a3aa3301cae670125f8b9d8cd8
Billy Le <GetSomePho@iCloud.com>
2015-10-23 01:14:04 -0400

    Removed previous temporary configuration

--- app/config/config.php
+++ app/config/config.php
@@ -8,16 +8,6 @@ $connectionString = "mysql:host=" . DBSERVER . ";dbname=" . DBNAME;
 
 define("DBCONNECTSTRING", $connectionString);
 
-
-// Used for inserting title, ** Will find different way later**
-try {
-    $sql = new PDO($connectionString, DBUSER, DBPASSWORD);
-    $sql->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo 'Unable to connect to the database server. <br />ERROR MESSAGE:<br />' . $e->getMessage();
-    exit();
-}
-
 //show errors
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
