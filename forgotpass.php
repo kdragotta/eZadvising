c5e8b72f7bf4ea0d94f71423cf014d75800e2b07
John <jtbertzfi@g.coastal.edu>
2015-10-29 17:00:33 -0400

    forgotpass.php update ************************** updated forgotpass.php because of system errors. *fixed connection problem 80% completed

--- forgotpass.php
+++ forgotpass.php
@@ -13,6 +13,9 @@ $showform = 1;
 $errorMessage = '';
 
 if(isset($_POST['submit'])){
+
+    $FORMFIELD['email'] = $_POST['email'];
+
     if(empty($FORMFIELD['email'])){
         $uid = FALSE;
         $errorMessage .= 'Please enter your email address';
@@ -26,20 +29,26 @@ if(isset($_POST['submit'])){
         echo 'in if: line 20';
     }
     else {
-            echo 'in else:21';
+        echo 'in else:21';
 
-            /*if (empty($_POST['email'])) {
-                $uid = FALSE;
-                $errorMessage .= 'Please enter your account email address.';
-            } else {
-              */
-                //check to see if email address is in database
-                $stmt = 'SELECT email FROM accounts WHERE email = :email';
-                $s = $pdo->prepare($stmt);
-                $s->bindValue(':email', $_POST['email']);
-                $s->execute();
-                $row = $s->fetch(PDO::FETCH_ASSOC);
+        /*if (empty($_POST['email'])) {
+            $uid = FALSE;
+            $errorMessage .= 'Please enter your account email address.';
+        } else {
+          */
+        //check to see if email address is in database
 
+
+
+        $conn = new PDO(DBCONNECTSTRING, DBUSER, DBPASSWORD);
+        $stmt = 'SELECT email FROM accounts WHERE email = :email';
+        $s = $conn->prepare($stmt);
+        $s->bindValue(':email', $_POST['email']);
+        $s->execute();
+        $row = $s->fetch(PDO::FETCH_ASSOC);
+
+
+        echo "past the stmt";
         if (empty($row['email'])) {
             $errorMessage .= 'The email you provided is not recognized.';
             echo '<p>Error</p>';
@@ -51,9 +60,9 @@ if(isset($_POST['submit'])){
     }
     if ($uid) {
         echo 'in if: 44';
-               //create a new, random password
+        //create a new, random password
         $p = substr(md5(uniqid(rand(), 1)), 3, 10);
-               //create query
+        //create query
         $stmt = 'UPDATE accounts SET resetpassword = :password WHERE email = :email';
         $s = $pdo->prepare($stmt);
         $s->bindValue(':password', $p);
@@ -63,7 +72,7 @@ if(isset($_POST['submit'])){
 
         if ($count == 1) {
             echo 'in if; send email: 58';
-                   //send email
+            //send email
             $body = 'Your password to log into eZadvising has been temporarirly changed to' . $p . '. Please log in using this password and your email. At that time you may change your password to something more familiar.';
             mail($_POST['email'], 'Your temporary password.', $body, 'From: admin@eZadvising.com');
             echo '<h3>Your password has been changed. You will receive the new, temporary password at the email address with which you provided.</h3>';
@@ -94,7 +103,7 @@ if($showform == 1){
                 <tr>
                     <th><lable for="email">Email:</lable></th>
                     <td><input type="text" name="email" size="20" maxlength="40"
-                    value="<?php if(isset($_POST['email'])) echo $_POST['email'];?>"/></td>
+                               value="<?php if(isset($_POST['email'])) echo $_POST['email'];?>"/></td>
                 </tr>
                 <tr>
                     <th>SUBMIT:</th>
