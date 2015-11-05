251258b383174d630e2cc3fc6cc07fc42e8026ea
Billy Le <GetSomePho@iCloud.com>
2015-11-05 00:24:33 -0500

    Refactored code, remember to update database with new ddl

---
+++ ScrapCode.js
@@ -0,0 +1,64 @@
+/**
+ * File containing scrap code, may be needed for later...
+ * @constructor
+ */
+
+function CustomHover() {
+    var defaultColor = 'black';
+
+    // Change color of last active tab
+    // $('.nav-pills .active a').css('background-color', 'blue');
+    $('#hover0').hover(function () {
+        $(this).css("background-color", colors[0]);
+    }, function () {
+        $(this).css("background-color", defaultColor);
+    });
+
+    $('#hover1').hover(function () {
+        $(this).css("background-color", colors[1]);
+    }, function () {
+        $(this).css("background-color", defaultColor);
+    });
+
+    $('#hover2').hover(function () {
+        $(this).css("background-color", colors[2]);
+    }, function () {
+        $(this).css("background-color", defaultColor);
+    });
+
+    $('#hover3').hover(function () {
+        $(this).css("background-color", colors[3]);
+    }, function () {
+        $(this).css("background-color", defaultColor);
+    });
+
+    $('#hover4').hover(function () {
+        $(this).css("background-color", colors[4]);
+    }, function () {
+        $(this).css("background-color", defaultColor);
+    });
+
+    $('#hover5').hover(function () {
+        $(this).css("background-color", colors[5]);
+    }, function () {
+        $(this).css("background-color", defaultColor);
+    });
+
+    $('#hover6').hover(function () {
+        $(this).css("background-color", colors[6]);
+    }, function () {
+        $(this).css("background-color", defaultColor);
+    });
+
+    $('#hover7').hover(function () {
+        $(this).css("background-color", colors[7]);
+    }, function () {
+        $(this).css("background-color", defaultColor);
+    });
+
+    $('#hover8').hover(function () {
+        $(this).css("background-color", colors[8]);
+    }, function () {
+        $(this).css("background-color", defaultColor);
+    });
+}
